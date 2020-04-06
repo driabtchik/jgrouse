56a12b93a76e3af41abd72102121c02b63c3627e
denis.riabtchik <denis.riabtchik@gmail.com>
2020-04-05 23:15:29 -0400

    preparing multi-project build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,13 @@
-
 import org.gradle.api.JavaVersion.VERSION_1_8
 
 plugins {
-    `java-library`
-    `maven-publish`
     id("pl.allegro.tech.build.axion-release") version "1.11.0"
-    id("com.jfrog.bintray") version "1.8.4"
-    id("org.ajoberstar.grgit") version "3.1.1" apply (false)
+    id("com.jfrog.bintray") version "1.8.4" apply false
+    id("org.ajoberstar.grgit") version "3.1.1" apply false
 }
 
 allprojects {
     group = "com.jgrouse"
-    apply(plugin = "java")
-
     repositories {
         jcenter()
         mavenCentral()
@@ -23,28 +18,36 @@ scmVersion {}
 
 subprojects {
     apply(plugin = "java-library")
+    apply(plugin = "java")
+
     apply(plugin = "jacoco")
     apply(plugin = "pl.allegro.tech.build.axion-release")
     apply(plugin = "com.jfrog.bintray")
     apply(plugin = "org.ajoberstar.grgit")
-    java {
-        sourceCompatibility = VERSION_1_8
-        targetCompatibility = VERSION_1_8
-        withSourcesJar()
-        withJavadocJar()
+
+
+    plugins.withType<JavaLibraryPlugin> {
+        extensions.configure<JavaPluginExtension> {
+            sourceCompatibility = VERSION_1_8
+            targetCompatibility = VERSION_1_8
+            withSourcesJar()
+            withJavadocJar()
+
+            dependencies {
+                val junitVer = "5.6.0"
+                "implementation"("javax.validation:validation-api:2.0.1.Final")
+                "implementation"("org.apache.commons:commons-lang3:3.9")
+
+                "testImplementation"("org.junit.jupiter:junit-jupiter-api:${junitVer}")
+                "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:${junitVer}")
+
+                "testImplementation"("org.assertj:assertj-core:3.15.0")
+                "testImplementation"("org.mockito:mockito-core:3.3.0")
+            }
+        }
     }
 
-    dependencies {
-        val junitVer = "5.6.0"
-        implementation("javax.validation:validation-api:2.0.1.Final")
-        implementation("org.apache.commons:commons-lang3:3.9")
 
-        testImplementation("org.junit.jupiter:junit-jupiter-api:${junitVer}")
-        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitVer}")
-
-        testImplementation("org.assertj:assertj-core:3.15.0")
-        testImplementation("org.mockito:mockito-core:3.3.0")
-    }
     version = scmVersion.version
 
     tasks.named<Test>("test") {
@@ -74,18 +77,15 @@ subprojects {
         }
     }
 
-    afterEvaluate {
-        val publishedArtifactId = ext["publishedArtifactId"] as String
-        val publishedArtifactName = ext["publishedArtifactName"] as String
-        val publishedArtifactDesc = ext["publishedArtifactDesc"] as String
-        val directorySuffix = ext["directorySuffix"] as String
-        setupPublishing(publishedArtifactId, publishedArtifactName, publishedArtifactDesc, directorySuffix)
-    }
-
     tasks.register("artifactsUpload") {
         dependsOn("build")
         if (ext["uploadAllowed"] as Boolean) {
-            finalizedBy(tasks.bintrayUpload)
+            val publishedArtifactId = ext["publishedArtifactId"] as String
+            val publishedArtifactName = ext["publishedArtifactName"] as String
+            val publishedArtifactDesc = ext["publishedArtifactDesc"] as String
+            val directorySuffix = ext["directorySuffix"] as String
+            setupPublishing(publishedArtifactId, publishedArtifactName, publishedArtifactDesc, directorySuffix)
+            finalizedBy(tasks.getByName("bintrayUpload"))
         } else {
             logger.info("No released artifacts are eligible for upload for ${project.name}")
         }
@@ -99,35 +99,37 @@ fun findProperty(s: String) = project.findProperty(s) as String?
 
 fun setupPublishing(publishedArtifactId: String,
                     publishedArtifactName: String,
-                    publishedArtifactDesc : String,
-                    directorySuffix: String)  {
-    publishing {
-        publications {
-            create<MavenPublication>("default") {
-                groupId = project.group as String
-                artifactId = publishedArtifactId
-                version = project.version as String
-                from(components["java"])
-                pom.withXml {
-                    asNode().apply {
-                        appendNode("name", publishedArtifactName)
-                        appendNode("description", publishedArtifactDesc)
-                        appendNode("url", "https://github.com/driabtchik/jgrouse/$directorySuffix")
-                        appendNode("licenses").appendNode("license").apply {
-                            appendNode("name", "The Apache Software License, Version 2.0")
-                            appendNode("url", "http://www.apache.org/licenses/LICENSE-2.0.txt")
-                            appendNode("distribution", "repo")
-                        }
-                        appendNode("developers").appendNode("developer").apply {
-                            appendNode("name", "Denis Riabtchik")
-                            appendNode("email", "denis.riabtchik@gmail.com")
-                            appendNode("organization", "jGrouse")
-                            appendNode("organizationUrl", "http://jGrouse.com")
-                        }
-                        appendNode("scm").apply {
-                            appendNode("connection", "scm:git:git://github.com/driabtchik/jgrouse.git")
-                            appendNode("developerConnection", "scm:git:ssh://github.com/driabtchik/jgrouse.git")
+                    publishedArtifactDesc: String,
+                    directorySuffix: String) {
+    plugins.withType<MavenPublishPlugin> {
+        extensions.configure<PublishingExtension> {
+            publications {
+                create<MavenPublication>("default") {
+                    groupId = project.group as String
+                    artifactId = publishedArtifactId
+                    version = project.version as String
+                    from(components["java"])
+                    pom.withXml {
+                        asNode().apply {
+                            appendNode("name", publishedArtifactName)
+                            appendNode("description", publishedArtifactDesc)
                             appendNode("url", "https://github.com/driabtchik/jgrouse/$directorySuffix")
+                            appendNode("licenses").appendNode("license").apply {
+                                appendNode("name", "The Apache Software License, Version 2.0")
+                                appendNode("url", "http://www.apache.org/licenses/LICENSE-2.0.txt")
+                                appendNode("distribution", "repo")
+                            }
+                            appendNode("developers").appendNode("developer").apply {
+                                appendNode("name", "Denis Riabtchik")
+                                appendNode("email", "denis.riabtchik@gmail.com")
+                                appendNode("organization", "jGrouse")
+                                appendNode("organizationUrl", "http://jGrouse.com")
+                            }
+                            appendNode("scm").apply {
+                                appendNode("connection", "scm:git:git://github.com/driabtchik/jgrouse.git")
+                                appendNode("developerConnection", "scm:git:ssh://github.com/driabtchik/jgrouse.git")
+                                appendNode("url", "https://github.com/driabtchik/jgrouse/$directorySuffix")
+                            }
                         }
                     }
                 }
@@ -135,24 +137,27 @@ fun setupPublishing(publishedArtifactId: String,
         }
     }
 
-    bintray {
-        user = findProperty("bintrayUser")
-        key = findProperty("bintrayKey")
-        publish = true
-        setPublications("default")
-        pkg(delegateClosureOf<com.jfrog.bintray.gradle.BintrayExtension.PackageConfig>{
-            repo = "jGrouse"
-            name = publishedArtifactId
-            userOrg = "jgrouse"
-            vcsUrl = "https://github.com/driabtchik/jgrouse.git"
-            setLabels("java")
-            setLicenses("Apache-2.0")
-        })
+
+    plugins.withType<com.jfrog.bintray.gradle.BintrayPlugin> {
+        extensions.configure<com.jfrog.bintray.gradle.BintrayExtension> {
+            user = findProperty("bintrayUser")
+            key = findProperty("bintrayKey")
+            publish = true
+            setPublications("default")
+            pkg(delegateClosureOf<com.jfrog.bintray.gradle.BintrayExtension.PackageConfig> {
+                repo = "jGrouse"
+                name = publishedArtifactId
+                userOrg = "jgrouse"
+                vcsUrl = "https://github.com/driabtchik/jgrouse.git"
+                setLabels("java")
+                setLicenses("Apache-2.0")
+            })
+        }
     }
+
 }
 
 tasks.register("artifactsUpload") {
-    dependsOn("build")
 }
 
 tasks.register("ciBuild") {
@@ -160,7 +165,7 @@ tasks.register("ciBuild") {
 }
 
 
-fun artifactUploadAllowed() : Boolean {
+fun artifactUploadAllowed(): Boolean {
     val grGit = org.ajoberstar.grgit.Grgit.open(mapOf("dir" to project.rootDir))
     val isClean = grGit.status().isClean
     return (isClean && !scmVersion.version.contains("SNAPSHOT"))
